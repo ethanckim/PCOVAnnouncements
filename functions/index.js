7798e08369c9dd708902d38f3dbea972d443c823
Ethan Kim <robhn38426@gmail.com>
2019-06-04 21:34:26 -0700

    latest for Capstone presentation

--- functions/index.js
+++ functions/index.js
@@ -3,31 +3,69 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 /**
- * 
- */
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    console.log("Hello World!")
-    response.send("Hello World!");
-});
-
-/**
  * Triggers when a new document has been write in the 
  * Firestore Announcements collection.
- *
- * Followers add a flag to `/followers/{followedUid}/{followerUid}`.
- * Users save their device notification tokens to `/users/{followedUid}/notificationTokens/{notificationToken}`.
  */
-// Listen for changes in all documents in the 'Announcements' collection
 exports.sendNewAnnouncementNotification = functions.firestore
     .document('Announcements/{date}')
-    .onCreate((snap, context) => {
-        const context = snap.data.context;
-        const type = snap.data.type;
-        const date = snap.data.date;
-        const position = snap.data.position;
+    .onCreate(async (snap, context) => {
+        const notcontext = snap.data.context;
+        const nottype = snap.data.type;
+        const notdate = snap.data.date;
 
-        //TODO Get the token from the database/firestore
-        const token =
-        //TODO send FCM message
+        // Get the token from the database/firestore
+        const getDeviceTokensPromise = admin.firestore().collection("Tokens").get();
+        // The snapshot to the user's tokens.
+        let tokensSnapshot;
+        // The array containing all the user's tokens.
+        let tokens;
+
+        const results = await Promise.all([getDeviceTokensPromise]);
+        tokensSnapshot = results[0];
+
+        // Check if there are any device tokens.
+        if (!tokensSnapshot.hasChildren()) {
+            return console.log('There are no notification tokens to send to.');
+        }
+        console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+
+        // Listing all tokens as an array.
+        tokens = Object.keys(tokensSnapshot.val());
+
+        //Message Details
+        const message;
+        if (nottype === 'announce') {
+            message = {
+                title: '주일 광고 ' + notdate,
+                body: notcontext
+            }
+        } else if (nottype === 'emergency') {
+            message = {
+                title: '비상 광고: ' + notcontext,
+                body: notcontext
+            } 
+        } else {
+            message = {
+                title: '광고: ' + notdate,
+                body: notcontext
+            }
+        }
+
+        // Send notifications to all tokens.
+        const response = await admin.messaging().sendToDevice(tokens, message);
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore.
+                if (error.code === 'messaging/invalid-registration-token' ||
+                    error.code === 'messaging/registration-token-not-registered') {
+                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove);
 
     });
