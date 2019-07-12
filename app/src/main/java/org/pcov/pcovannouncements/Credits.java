c5d97bc486861e0e910133e8e4ac4703d4b96af9
Ethan Kim <robhn38426@gmail.com>
2019-07-12 14:25:06 -0700

    Gallery functionality polishing
    Image Download notifications, progress bar, and a really bad credit page. Removed all of the tempeory images, and there are other small impovements here and there.

---
+++ app/src/main/java/org/pcov/pcovannouncements/Credits.java
@@ -0,0 +1,33 @@
+package org.pcov.pcovannouncements;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // this takes the user 'back', as if they pressed the back button on the main android toolbar.
+                super.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
